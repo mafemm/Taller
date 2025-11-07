9df9989837d6bf5e17fa66b0264006890694889e
mafemm <mafemorenom09@gmail.com>
2025-11-07 17:00:49 -0500

    Add files via upload

---
+++ assets/models/mensajes.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+/*
+const UsuarioSchema = new mongoose.Schema({
+    cedula:String,
+    userName:String,
+    nombre:String,
+    apellido:String,
+    correo:String,
+    password:String,
+    telefono: int
+},{collection:"usuarios"})
+*/
+
+const MensajeSchema = new mongoose.Schema({
+    mensaje:String
+},{collection:"mensajes"})
+
+mongoose.model("mensajes",MensajeSchema)
